e6ff2ad29ace690183e8cf4a25b5b8946bb13dd7
aanandkumar799 <aanandkumar799@gmail.com>
2026-09-16 17:44:55 +0530

    Cache the practice quiz in the PWA shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'dravya-guna-97-v2';
-const APP_SHELL = ['./','./index.html','./plants.html','./plant.html','./compare.html','./style.css','./script.js','./plant-dossier-layout.css','./academic-enhancements.css','./academic-enhancements.js','./manifest.json','./404.html','./robots.txt','./sitemap.xml'];
+const CACHE_NAME = 'dravya-guna-97-v3';
+const APP_SHELL = ['./','./index.html','./plants.html','./plant.html','./compare.html','./quiz.html','./style.css','./script.js','./plant-dossier-layout.css','./academic-enhancements.css','./academic-enhancements.js','./manifest.json','./404.html','./robots.txt','./sitemap.xml'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{const r=event.request;if(r.method!=='GET')return;const u=new URL(r.url);if(u.origin!==self.location.origin)return;event.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{if(res&&res.status===200&&res.type==='basic'){const copy=res.clone();caches.open(CACHE_NAME).then(cache=>cache.put(r,copy))}return res}).catch(()=>caches.match('./index.html'))))});
